71fd3269c5e43c91ad614e1b932f22f6c24be97f
Lucas Sowul <sowul.lukas@gmail.com>
2023-01-14 12:46:33 +0100

    fill array with random numbers specified in constructor

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+/*
+fill array with random numbers specified in constructor
+ */
+public class Main {
+    public static void main(String[] args) {
+        int [] randNumbers = new int[20];
+        Random rClass = new Random();
+        for (int i = 0; i < randNumbers.length; i++) {
+            randNumbers [i] = rClass.nextInt();
+        }
+        for (int i = 0 ; i<randNumbers.length; i++) {
+            System.out.print(randNumbers[i] + " ");
+        }
+    }
+}
+
